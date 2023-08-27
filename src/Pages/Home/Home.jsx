80053c305a341f7e6635d0cb6407a7087430b62e
Amal C P <acp070146@gmail.com>
2023-08-27 21:48:33 +0530

    banner updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import DynamicParaBox from '../../Components/HomeComponents/DynamicParaBox/DynamicParaBox'
+import React from "react";
+import DynamicParaBox from "../../Components/HomeComponents/DynamicParaBox/DynamicParaBox";
+import Banner from "../../Components/CommunityLogoScroller/Banner";
+import HorizCardGrp from "../../Components/HomeComponents/HorizCardGrp/HorizCardGrp";
+
+const imageCount = 5;
+const images = new Array(imageCount)
+  .fill(`../../../public/logos`)
+  .map((image, ix) => ({
+    id: crypto.randomUUID(),
+    image: `${image}${ix + 1}.png`,
+  }));
 
 const Home = () => {
   return (
     <div>
+      <Banner images={images} speed={8000} />
+      <HorizCardGrp />
       <DynamicParaBox />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
