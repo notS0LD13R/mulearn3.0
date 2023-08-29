d011610ab023cb0c85431805fe42faa858e303b6
Amal C P <acp070146@gmail.com>
2023-08-29 23:31:22 +0530

    redirection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-
-import Home from './Pages/Home/Home'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Carrers from "./Pages/Carrers/Carrers";
 
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/carrers",
+      element: <Carrers />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
